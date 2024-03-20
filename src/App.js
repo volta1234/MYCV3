f98d1062f313f8419e5d6f275d12729d6eacf25f
CALVINCE <calvince.ogwel@gmail.com>
2024-03-20 15:48:21 +0300

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Cvinput from "./cvinput/cvinput";
+import Mycv from "./mycv/mycv";
+import Navbar from "./navbar";
+import Profile from "./cvinput/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/cvinput" element={<Cvinput />} />
+        <Route path="/" element={<Mycv/>}/>
+        <Route path="/profile" element={<Profile/>} />
+      </Routes>
     </div>
   );
 }
